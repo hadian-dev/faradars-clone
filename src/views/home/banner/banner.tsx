f56b17aa05fa633ff43ca90a949c5885f34a4bc7
MohammadHadiyan <36923502+MohamadHadiyan@users.noreply.github.com>
2023-05-02 22:37:57 +0430

    feat: create home view banner

---
+++ src/views/home/banner/banner.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Features from './features'
+import { Search } from '@/components/organisms'
+
+const Banner = () => {
+  return (
+    <div
+      className='relative bg-cover'
+      style={{ backgroundImage: `url("/images/bookcase.jpg")` }}
+    >
+      <div className='w-full h-full bg-gray-900/30 text-white backdrop-blur-sm md:backdrop-blur'>
+        <div className='max-w-[1200px] m-auto md:h-96 p-4 flex gap-4 flex-col justify-center items-center'>
+          <p className='text-center w-full sm:w-3/4'>
+            در بین بیش از 20,000 ساعت آموزش فرادرسی جستجو کنید و به جمع میلیونی
+            دانشجویان فرادرسی بپیوندید.
+          </p>
+          <div className='flex gap-4 items-center w-full p-2 sm:w-3/4 max-w-xl'>
+            <Search />
+            <button className='bg-green-600 py-2 px-4 rounded-md text-white'>
+              جستجو
+            </button>
+          </div>
+          <Features />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Banner
