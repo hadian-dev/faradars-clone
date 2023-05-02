efa2f460368cbbc6bfbb5a520d20c0684b0ba405
MohammadHadiyan <36923502+MohamadHadiyan@users.noreply.github.com>
2023-05-02 22:40:56 +0430

    chore: modify tailwind  and ts configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontSize: {
+        xsm: '0.75rem',
+      },
+      fontFamily: {
+        shabnam: ['Shabnam', 'sans-serif'],
       },
     },
   },
