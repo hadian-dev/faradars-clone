b8e6765ab7b02ae042317faa318e444f45136011
MohammadHadiyan <36923502+MohamadHadiyan@users.noreply.github.com>
2023-05-02 22:34:34 +0430

    feat: create shared icon components

---
+++ src/components/shared/icons/index.ts
@@ -0,0 +1,4 @@
+export { MenuIcon } from './menu'
+export { UserCircleIcon } from './user-circle'
+export { FaradarsLogoIcon } from './faradars-logo'
+export { SearchIcon } from './search'
---
+++ src/components/shared/icons/user-circle.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export function UserCircleIcon() {
+  return (
+    <svg
+      width='40'
+      height='40'
+      viewBox='0 0 40 40'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <circle
+        cx='20'
+        cy='20'
+        r='20'
+        fill='#1E88E5'
+        fill-opacity='0.08'
+      ></circle>
+      <g clip-path='url(#clip0_116_1805)'>
+        <path
+          d='M20.1562 20C22.5727 20 24.5312 18.0412 24.5312 15.625C24.5312 13.2088 22.5727 11.25 20.1562 11.25C17.7397 11.25 15.7812 13.2088 15.7812 15.625C15.7812 18.0412 17.7397 20 20.1562 20ZM21.8891 21.6406H18.4233C15.1527 21.6406 12.5 24.293 12.5 27.564C12.5 28.2182 13.0304 28.749 13.6846 28.749H26.6285C27.2827 28.75 27.8125 28.2202 27.8125 27.564C27.8125 24.293 25.1601 21.6406 21.8891 21.6406Z'
+          fill='#B1BAC4'
+        ></path>
+      </g>
+      <defs>
+        <clipPath id='clip0_116_1805'>
+          <rect
+            width='15.3125'
+            height='17.5'
+            fill='white'
+            transform='translate(12.5 11.25)'
+          ></rect>
+        </clipPath>
+      </defs>
+    </svg>
+  )
+}
