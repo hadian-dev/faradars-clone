5f24114639fcbc77f393e9ddaae85d81e8ef2622
MohammadHadiyan <36923502+MohamadHadiyan@users.noreply.github.com>
2023-05-02 22:38:33 +0430

    feat: create home view category-list

---
+++ src/views/home/category-list/category-list.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const CategoryList = () => {
+  return (
+    <div className='max-w-[1200px] m-auto'>
+      <div className='pt-3 md:pt-8'>
+        <header>
+          <h1 className='text-center text-2xl p-2'>
+            موضوعات و محورهای آموزشی منتخب
+          </h1>
+        </header>
+      </div>
+    </div>
+  )
+}
+
+export default CategoryList
---
+++ src/views/home/index.tsx
@@ -0,0 +1,13 @@
+import Banner from './banner'
+import CategoryList from './category-list'
+
+function HomeView() {
+  return (
+    <div>
+      <Banner />
+      <CategoryList />
+    </div>
+  )
+}
+
+export default HomeView
